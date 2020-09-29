f54b4c34d29f5d6c091ccc319389609873ebeb8a
Igor <igor1994ua@gmail.com>
2020-09-29 10:06:08 +0300

    Убраны не нужные зависимости.

--- HomeTask4.Core/Entities/EntityMenu.cs
+++ HomeTask4.Core/Entities/EntityMenu.cs
@@ -1,9 +1,8 @@
-﻿using HomeTask4.SharedKernel;
-
-namespace HomeTask4.Core.Entities
+﻿namespace HomeTask4.Core.Entities
 {
-    public class EntityMenu : BaseEntity
+    public class EntityMenu
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int ParentId { get; set; }
         public string TypeEntity { get; set; }
--- HomeTask4.Core/Entities/Ingredient.cs
+++ HomeTask4.Core/Entities/Ingredient.cs
@@ -1,5 +1,4 @@
 ﻿using HomeTask4.SharedKernel;
-using System.ComponentModel.DataAnnotations;
 
 namespace HomeTask4.Core.Entities
 {
