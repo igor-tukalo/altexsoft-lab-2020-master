6a2b78f2c4fb72f227de5b9a6941fdc16a497609
Igor <igor1994ua@gmail.com>
2020-10-08 08:59:53 +0300

    Исправление комментариев.

--- HomeTask4.Cmd/ValidationNavigation.cs
+++ HomeTask4.Cmd/ValidationNavigation.cs
@@ -7,7 +7,7 @@ namespace HomeTask4.Cmd
     public class ValidationNavigation : IValidationNavigation
     {
         /// <summary>
-        /// Obtain a Y or N response
+        /// Obtain a Y or N response.
         /// </summary>
         /// <returns>response</returns>
         public Task<ConsoleKey> ShowYesNoAsync()
@@ -15,32 +15,33 @@ namespace HomeTask4.Cmd
             ConsoleKey response;
             do
             {
-                while (Console.KeyAvailable) // Flushes the input queue.
+                while (Console.KeyAvailable)
                 {
                     Console.ReadKey();
                 }
                 Console.Write("    y or n? ");
                 response = Console.ReadKey().Key;
                 Console.WriteLine();
-            } while (response != ConsoleKey.Y && response != ConsoleKey.N); // If the user did not respond with a 'Y' or an 'N', repeat the loop.
+            } while (response != ConsoleKey.Y && response != ConsoleKey.N);
             return Task.FromResult(response);
         }
 
         /// <summary>
-        /// Wrapping text on a new line after n characters
+        /// Wrapping text on a new line after n characters.
         /// </summary>
-        /// <param name="numChar">number of characters in one line</param>
-        /// <param name="text">number of characters</param>
+        /// <param name="numberChar">number of characters in one line</param>
+        /// <param name="text">wrapping text</param>
         /// <param name="wrapChar">indent mark</param>
-        public Task<string> WrapTextAsync(int numChar, string text, string wrapChar = "\n")
+        /// <returns>wrapping text</returns>
+        public Task<string> WrapTextAsync(int numberChar, string text, string wrapChar = "\n")
         {
             if (!string.IsNullOrWhiteSpace(text))
             {
                 string[] wrapText = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-                if (wrapText.Length > numChar)
+                if (wrapText.Length > numberChar)
                 {
-                    for (int i = numChar - 1; i < wrapText.Length; i += numChar)
+                    for (int i = numberChar - 1; i < wrapText.Length; i += numberChar)
                     {
                         wrapText[i] += wrapChar;
                     }
@@ -51,10 +52,10 @@ namespace HomeTask4.Cmd
         }
 
         /// <summary>
-        /// Сheck for text emptiness
+        /// Check text for emptiness.
         /// </summary>
         /// <param name="text"></param>
-        /// <returns></returns>
+        /// <returns>text</returns>
         public Task<string> CheckNullOrEmptyTextAsync(string text)
         {
             do
@@ -70,26 +71,26 @@ namespace HomeTask4.Cmd
         }
 
         /// <summary>
-        /// Check for batch availability
+        /// Check batch for availability among all batches.
         /// </summary>
-        /// <param name="batch"></param>
-        /// <param name="countBatch"></param>
-        /// <returns></returns>
-        public Task<int> BatchExistAsync(int batch, int countBatch)
+        /// <param name="numberBatch">batch number to check</param>
+        /// <param name="countBatch">total batch</param>
+        /// <returns>number batch</returns>
+        public Task<int> BatchExistAsync(int numberBatch, int countBatch)
         {
             try
             {
-                while (batch < 1 || batch > countBatch)
+                while (numberBatch < 1 || numberBatch > countBatch)
                 {
                     Console.Write("    The page number does not exist! Enter page number: ");
-                    batch = int.Parse(Console.ReadLine());
+                    numberBatch = int.Parse(Console.ReadLine());
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            return Task.FromResult(batch);
+            return Task.FromResult(numberBatch);
         }
     }
 }
